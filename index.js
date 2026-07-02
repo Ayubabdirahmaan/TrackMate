e92ec1bd43bb4eba91eb8d28d70e78f55c0d58be
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-02 15:09:59 +0300

    feat: add user Authantication

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-import express from 'express'
-const app = express()
-import dotenv from 'dotenv'
-dotenv.config()
-const PORT = process.env.PORT
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT;
+import mongoose from "mongoose";
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello world this page test");
+});
 
-app.use(express.json())
+const mongooseUri =
+  process.env.NODE_ENV == "development"
+    ? process.env.MONGO_URI_DEV
+    : MONGO_URI_PRO;
 
-app.get('/', (req, res) => {
-    res.send('hello world this page test')
-})
+mongoose
+  .connect(mongooseUri)
+  .then(() => console.log("✅ MongoDB connected Successfully"))
+  .catch((error) => console.log("❌ Connection err", error));
 
-app.listen(PORT, ()=> {
-    console.log(`server is runing on https://localhost:5000 ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server is runing on https://localhost:5000 ${PORT}`);
+});
---
+++ model/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: {type: String, unique: true},
+    password: String,
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    profile: String
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
