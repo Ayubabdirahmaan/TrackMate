cdb7c378409c9735fc220a962e18cc8aa5f6966c
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-03 21:19:28 +0300

    feat: add database model

--- model/User.js
+++ model/User.js
@@ -13,6 +13,18 @@ const userSchema = new mongoose.Schema({
     profile: String
 })
 
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next()
+
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+});
+
+userSchema.methods.comparePassword = function (inputPassword) {
+    return bcrypt.compare(inputPassword, this.password)
+}
+
+
 
 
 
