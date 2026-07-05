83f748279d36b7300def6b6a5359e5d857549011
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-05 13:11:15 +0300

    fix token with login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import User from "../model/User.js";
+import { generateToken } from "../utils/generateToken.js";
 export const register = async (req, res, next) => {
   const { name, email, password, role, profilePic } = req.body;
 
@@ -9,20 +10,22 @@ export const register = async (req, res, next) => {
         message: "Email already in use",
       });
     const user = await User.create({ name, email, password, role, profilePic });
-    res.status(201).json(user);
+    const token = generateToken(user._id);
+    res.status(201).json({ token });
   } catch (error) {
     next(error);
   }
 };
 
 export const login = async (req, res, next) => {
-    let {email, password} = req.body
-    try {
-        const user = await User.findOne({email})
-        if(!user) return res.status(401).json({message: 'Invalid email or password'})
-            res.json(user.name)
-        
-    } catch (error) {
-        next(error)
-    }
-}
+  let { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password)))
+      return res.status(401).json({ message: "Invalid email or password" });
+    const token = generateToken(user._id);
+    res.json({ token, user });
+  } catch (error) {
+    next(error);
+  }
+};
