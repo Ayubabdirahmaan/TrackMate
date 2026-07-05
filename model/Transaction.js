93ef022f07f2523769662d1d9a114d6e22fc87f1
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-05 14:17:22 +0300

    feat: add model of transactions

---
+++ model/Transaction.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const transactionSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: true },
+    amount: {
+      type: Number,
+      require: true,
+    },
+    type: {
+      type: String,
+      enum: ["expense", "income"],
+      default: "expense",
+    },
+    category: { type: String, required: true },
+    transactionDate: Date,
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Transaction", transactionSchema);
