7b8a015cfb3c86283cbf712cdb99d7730f451100
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-06 10:34:31 +0300

    feat: add upload image

---
+++ controllers/uploadController.js
@@ -0,0 +1,22 @@
+import { success } from "zod"
+import User from "../model/User"
+import cloudinary from "../utils/cloudinary"
+
+export const uploadFile = (req, res, next) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded' })
+    }
+    const stream = cloudinary.uploader.upload_stream({
+        folder: 'personal_tracker', resource_type: 'auto'
+    },
+        async (error, result) => {
+            if (error) return next(error)
+            await User.findByIdAndUpdate(req.user._id, { profile: result.secure_url })
+            return res.status(201).json({
+                success: true,
+                fileUrl: result.secure_url
+            })
+        }
+    )
+    stream.end(req.file.buffer)
+}
