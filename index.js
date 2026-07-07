a7edcac994f47c2b72c71b792bd0fe9e4454073d
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-07 09:42:07 +0300

    feat: add swagger ui docs

--- index.js
+++ index.js
@@ -5,51 +5,55 @@ dotenv.config();
 const PORT = process.env.PORT;
 import mongoose from "mongoose";
 import morgan from "morgan";
-import cors from 'cors'
+import cors from "cors";
 import helmet from "helmet";
-import userRegister from './/Routes/auth.js'
-import userLogin from './/Routes/auth.js'
-import getAllUsers from './/Routes/auth.js'
-import adminDashbaord from './/Routes/admin.js'
-import transaction from './/Routes/transactions.js'
-import getTransaction from './/Routes/transactions.js'
-import updateTransaction from './/Routes/transactions.js'
-import deleteTransaction from './/Routes/transactions.js'
-import getMonthlySummary from './/Routes/transactions.js'
-import uploadFile from './/Routes/upload.js'
-
-
-import {notfound} from './middlewares/notfound.js'
+import swaggerUi from 'swagger-ui-express';
+import userRegister from ".//Routes/auth.js";
+import userLogin from ".//Routes/auth.js";
+import getAllUsers from ".//Routes/auth.js";
+import adminDashbaord from ".//Routes/admin.js";
+import transaction from ".//Routes/transactions.js";
+import getTransaction from ".//Routes/transactions.js";
+import updateTransaction from ".//Routes/transactions.js";
+import deleteTransaction from ".//Routes/transactions.js";
+import getMonthlySummary from ".//Routes/transactions.js";
+import uploadFile from ".//Routes/upload.js";
+import { notfound } from "./middlewares/notfound.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { limiter } from "./middlewares/rateLimiter.js";
+import { swaggerSpec } from "./utils/swagger.js";
 app.use(express.json());
 
-app.use(cors({
-  origin: ['http://localhost:5000']
-}))
-app.use(limiter)
-app.use(helmet())
+app.use(
+  cors({
+    origin: ["http://localhost:5000"],
+  }),
+);
+app.use(limiter);
+app.use(helmet());
 // middlewares
-app.use('/api/auth', userRegister )
-app.use('/api/auth', userLogin )
-app.use('/api/auth', getAllUsers )
-app.use('/api/admin', adminDashbaord )
-app.use('/api/transaction', transaction )
-app.use('/api/transaction', getTransaction )
-app.use('/api/transaction', updateTransaction )
-app.use('/api/transaction', deleteTransaction )
-app.use('/api/transaction', getMonthlySummary )
-app.use('/api/upload', uploadFile )
+app.use("/api/auth", userRegister);
+app.use("/api/auth", userLogin);
+app.use("/api/auth", getAllUsers);
+app.use("/api/admin", adminDashbaord);
+app.use("/api/transaction", transaction);
+app.use("/api/transaction", getTransaction);
+app.use("/api/transaction", updateTransaction);
+app.use("/api/transaction", deleteTransaction);
+app.use("/api/transaction", getMonthlySummary);
+app.use("/api/upload", uploadFile);
 
 app.get("/", (req, res) => {
   res.send("hello world this page test page");
 });
 
-app.use(notfound)
-app.use(errorHandler)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-if(process.env.NODE_ENV == "development") {
-  app.use(morgan('dev'))
+app.use(notfound);
+app.use(errorHandler);
+
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
 }
 
 const mongooseUri =
--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,6 +2,6 @@ import rateLimiter from 'express-rate-limit'
 
 export const limiter = rateLimiter({
     windowMs: 1 * 60 * 1000,
-    max: 3,
+    max: 100,
     message:  'Too many request.  Please try again later'
 })
