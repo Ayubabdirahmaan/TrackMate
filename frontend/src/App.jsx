97fff8439e9a95cfa3ea5a414f19fcc8d21926d3
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-08 09:46:12 +0300

    feat: add shadcn installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-
-import './App.css'
-
+import "./App.css";
+import {Button} from '@/components/ui/button'
 function App() {
-
-return (
-  <h1>Hello world</h1>
-)
-
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>Register</Button>
+    </>
+  );
 }
 
-export default App
+export default App;
