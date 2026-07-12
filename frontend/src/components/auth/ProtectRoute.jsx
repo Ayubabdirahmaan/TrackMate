a70552cbaa1fe5e0089425b44e77b19c2132b314
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-12 20:38:46 +0300

    feat: add protect route

--- frontend/src/components/auth/ProtectRoute.jsx
+++ frontend/src/components/auth/ProtectRoute.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import api from "@/lib/api/apiClient";
+import useAuthStore from "@/lib/store/authStore";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import React, { useEffect } from "react";
+import { Navigate } from "react-router";
 
-const ProtectRoute = () => {
-  return (
-    <div>ProtectRoute</div>
-  )
-}
+const ProtectRoute = ({ children }) => {
+  const { user, setAuth, clearAuth, token } = useAuthStore();
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async () => {
+      const response = await api.get("/auth/dashboard");
+      return response.data;
+    },
+    retry: 1,
+  });
+  // error case
+  useEffect(() => {
+    if (isError) {
+      clearAuth();
+    }
+  }, [isError, data, setAuth, token]);
 
-export default ProtectRoute
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectRoute;
