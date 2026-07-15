e479d50eadf3d3cf34d2b466477c8b0c8d41f12f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-07-15 10:24:11 +0300

    feat: add dashboard

--- frontend/src/pages/Dashbaord/Dashbaord.jsx
+++ frontend/src/pages/Dashbaord/Dashbaord.jsx
@@ -1,7 +1,18 @@
 import DashbaordHeader from '@/components/Dashbaord/DashbaordHeader'
-import React from 'react'
+import DashboardWelcome from '@/components/Dashbaord/DashboardWelcome'
+import React, { useState } from 'react'
 
 const Dashbaord = () => {
+
+  const [showCreateForm, setShowCreateForm] = useState(false)
+  const [edtingTask, setEdtingTask] = useState(null)
+
+  const handleCreateTask = () => {
+    setShowCreateForm(true)
+    setEdtingTask(null)
+  }
+
+
   return (
   
   <div className='min-h-screen bg-background'>
@@ -9,6 +20,12 @@ const Dashbaord = () => {
       <DashbaordHeader />
 
       {/* main */}
+      <main className='max-full px-4 py-8 space-y-6'>
+            <DashboardWelcome
+            showCreateForm={showCreateForm}
+            onCreateTask={handleCreateTask}
+             />
+      </main>
   </div>
   )
 }
